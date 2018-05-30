dce76b5a1bbd7e9a89a8068af3a2aec9532f079e
Nelcifran <nelcifranpires@gmail.com>
2018-05-30 11:28:07 -0300

    views for manager users added and routes too

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods
   resources :stores
+  resources :users
   root to: 'stores#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
